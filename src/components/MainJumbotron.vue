9e9f0a1180562feaddb121f0eb5dc47deb899704
Antonius131 <antomendetta@gmail.com>
2022-07-14 16:48:34 +0200

    MainJumbotron style

--- src/components/MainJumbotron.vue
+++ src/components/MainJumbotron.vue
@@ -1,13 +1,9 @@
 <template>
    <div>
-      <div class="p-5 rounded-2">
-         <div class="container-fluid">
-            <div class="row">
-               <div class="col-12">
-                  <h1>Join the Dark Side</h1>
-                  <p>We have Cookies</p>
-               </div>
-            </div>
+      <div id="jumbotron" class="p-5 rounded-1">
+         <div class="col-12">
+            <h1>Join the Dark Side</h1>
+            <p>We have Cookies</p>
          </div>
       </div>
    </div>
@@ -17,6 +13,30 @@
 
 </script>
 
-<style>
+<style scoped>
+   #jumbotron {
+      background-image: url(https://wallpaperaccess.com/full/3399933.jpg);
+      background-size: cover;
+      height: 300px;
+   }
 
+   .p-5 {
+      padding: 5rem;
+   }
+
+   .rounded-1 {
+      border-radius: 1rem;
+   }
+
+   h1 {
+      color: #fff;
+      font-size: 4rem;
+      margin-bottom: .5rem;
+   }
+
+   p {
+      font-size: 1.5rem;
+      color: #fff;
+      margin-top: .5rem;
+   }
 </style>
